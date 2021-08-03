854ab3b7caa2398c1c3f10248edbba1b3bdd5d57
elliefiddian <86102143+elliefiddian@users.noreply.github.com>
2021-08-03 07:32:00 +0100

    updated

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ const clap = new Audio("sound/clap.wav");
 const crash = new Audio("sound/crash.wav");
 const openhit = new Audio('sound/open-hithat.wav');
 const kick = new Audio('sound/kick.wav');
-const closedhit = new Audio('sound/closed-hithat.wav');
+const closedhit = new Audio('sound/closed-hit.wav');
 const snare = new Audio('sound/snare.wav');
 
 window.addEventListener('keydown', (e) =>{
     const keyCode = e.code;
     console.log(keyCode);
-}
+
 
 if (keyCode === "KeyA") {
     clap.play();
@@ -32,30 +32,54 @@ if (keyCode === "KeyA") {
 
 } else if (keyCode === "KeyH") {
     snare.play();
-
 }
+});
+
  
 
 window.addEventListener('click', (e) =>{
-    const keyCode = e.code;
-}
+    const button = e.code;
 
-if (keyCode === "KeyA") {
+if (button === "") {
     clap.play();
     
-} else if (keyCode === "KeyS") {
+} else if (button === "") {
     crash.play();
     
-} else if (keyCode === "KeyD") {
+} else if (button === "") {
     openhit.play();
     
-} else if (keyCode === "keyF") {
+} else if (button === "") {
     kick.play();
     
-} else if (keyCode === "KeyG") {
+} else if (button === "") {
     closedhit.play();
     
-} else if (keyCode === "keyH") {
+} else if (button === "") {
     snare.play();
+}
+});
 
+// this is not running on click?
+
+//styling
+const myHeading = document.getElementById ("heading");
+const list = document.getElementsByTagName ("li");
+const notPink = document.getElementsByClassName ("notPink");
+
+
+myHeading.addEventListener ("click", () =>{
+myHeading.style.color = "purple";
+})
+
+for (let i = 0; i<list.length; i++){
+  list [i].style.color = "purple";
+}
+
+for (let i = 0; i< notPink.length; i++){
+    notPink[i].style.color = "pink";
+} 
+
+function myFunction () {
+    document.getElementById("heading").innerHTML = "Get Ready to Play"; 
 }
